03089d184b6d606b0831ba4a6a29e88b3e5c5ed1
Chris Cadek <cc@wh-i.at>
2024-06-07 12:57:18 +0200

    Migrate nvim config to ccadek/kickstart.nvim

--- .config/nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "folke/noice.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-}
