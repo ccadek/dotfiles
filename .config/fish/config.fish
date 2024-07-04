041cdd0c180e4762fa9a3ec858ff9306000995d6
Chris Cadek <cc@wh-i.at>
2024-07-04 10:48:45 +0200

    restructures fish config
    * remove pyenv sourcing
    * group related statements together
    * cleanup of unused config
    * unify how environment variables are setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,41 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -gx EDITOR nvim
-
-set -gx MANPAGER "nvim +Man!"
-
 set fish_greeting ""
-
-# download lombok from https://projectlombok.org/download
-set -gx JDTLS_JVM_ARGS "-javaagent:$HOME/.local/share/java/lombok-1.18.32.jar"
-set -gx nvm_default_version "v16.3.0"
-set -gx ANDROID_SDK /home/purzel/Android/Sdk
-set -gx forgit_checkout_commit zzzzzzzzgcooooooo
-fish_add_path -g -p $HOME/fvm/default/bin
-fish_add_path -g -p $HOME/go/bin
-
-set -x PATH "$HOME/.cargo/bin" $PATH
-
-# docker config home
-# used to install docker-compose manually
-# check https://docs.docker.com/compose/install/linux/
-set -gx DOCKER_CONFIG "$HOME/.docker"
-
-# initialize pyenv
-pyenv init - | source
-
-# source fzf
-fzf --fish | source
-
-# prompt setting
-
-set -U hydro_color_git red
-set -U hydro_color_pwd green
-set -U hydro_color_prompt blue
-set -U hydro_symbol_git_dirty " ✗"
-
-alias fix_camera='guvcview -d /dev/video0'
 
 #
 # The next two settings are taken from https://www.reddit.com/r/fishshell/comments/176ahss/tip_you_can_use_function_subfolders_with_this/.
@@ -47,4 +13,30 @@ set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_func
 # add completion subdirs to fish_completion_path.
 set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
 
+
+set -gx ANDROID_SDK /home/purzel/Android/Sdk # download lombok from https://projectlombok.org/download
+set -gx DOCKER_CONFIG "$HOME/.docker"
+set -gx EDITOR nvim
 set -gx FZF_DEFAULT_OPTS "--bind up:preview-up,down:preview-down"
+set -gx JDTLS_JVM_ARGS "-javaagent:$HOME/.local/share/java/lombok-1.18.32.jar"
+set -gx MANPAGER "nvim +Man!"
+set -gx nvm_default_version "v16.3.0"
+set -gx PATH "$HOME/.cargo/bin" $PATH
+set -gx PATH "$HOME/fvm/default/bin" $PATH
+set -gx PATH "$HOME/go/bin" $PATH
+
+
+# prompt setting
+
+set -U hydro_color_git red
+set -U hydro_color_pwd green
+set -U hydro_color_prompt blue
+set -U hydro_symbol_git_dirty " ✗"
+
+
+# aliases
+
+alias fix_camera='guvcview -d /dev/video0'
+
+# sourcing
+fzf --fish | source
