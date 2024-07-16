6424e074f1a505c40370de91e18fcf2ec6de7d33
Chris Cadek <cc@wh-i.at>
2024-07-16 10:09:16 +0200

    zsh: adds fish like syntax highlighting

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -2,5 +2,6 @@
 
 source $ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $ZSH_PLUGIN_DIR/zsh-autopair/autopair.zsh
+source $ZSH_PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 autopair-init
