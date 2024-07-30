4f729ee6a58e0d576603e229932402b2c32bea6e
Chris Cadek <cc@wh-i.at>
2024-07-30 14:02:50 +0200

    nvim: removes default settings from catppuccin theme

--- .config/nvim/lua/plugins/ui/catppuccin.lua
+++ .config/nvim/lua/plugins/ui/catppuccin.lua
@@ -8,34 +8,6 @@ return {
         light = 'latte',
         dark = 'mocha',
       },
-      transparent_background = false, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = 'dark',
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
-      },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { 'italic' }, -- Change the style of comments
-        conditionals = { 'italic' },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-        -- miscs = {}, -- Uncomment to turn off hard-coded styles
-      },
-      highlight_overrides = {},
-      color_overrides = {},
       custom_highlights = function(colors)
         return {
           -- Both colors taken from folke/tokyonight theme.
@@ -43,19 +15,6 @@ return {
           LineNr = { fg = '#a8aecb' }, -- set all other line numbers to some shade of grey/blue. Apparently it is called 'Logan'
         }
       end,
-      default_integrations = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-          enabled = true,
-          indentscope_color = '',
-        },
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-      },
     }
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
