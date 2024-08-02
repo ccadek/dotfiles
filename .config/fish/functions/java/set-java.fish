f5d68c23d87ee615b0f9b5beebd7ea150a048fb7
Chris Cadek <purzelification@gmail.com>
2024-08-02 22:18:58 +0200

    nvim: adds description and removes useless lines from set-java

--- .config/fish/functions/java/set-java.fish
+++ .config/fish/functions/java/set-java.fish
@@ -1,13 +1,11 @@
-function set-java
+function set-java -d "Arch only. Set the currently active Java"
   find /usr/lib/jvm/java-* > /dev/null 2>&1
   if test $status -ne 0
     echo ""
     echo "No Java SDKs available under /usr/lib/jvm. Please install a JVM at that location"
     return 1
   end
-  set java_directories (find /usr/lib/jvm/java-* -maxdepth 0 -type d -printf "%f\n")
   set java (find /usr/lib/jvm/java-* -maxdepth 0 -type d -printf "%f\n" | fzf)
-  #echo "'$java' count $java"
   sudo archlinux-java set $java
   javac -version
 end
