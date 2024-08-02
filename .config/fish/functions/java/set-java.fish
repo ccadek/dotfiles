879430e7aa30875f3fa191ecc6503946d59a759c
Chris Cadek <purzelification@gmail.com>
2024-08-02 22:10:55 +0200

    fish: make set-java resilient
    If no JVM is installed in /usr/lib/jvm, then the script will exit early
    and print an error message

--- .config/fish/functions/java/set-java.fish
+++ .config/fish/functions/java/set-java.fish
@@ -1,5 +1,13 @@
 function set-java
-    set java (find /usr/lib/jvm/java-* -maxdepth 0 -type d -printf "%f\n" | fzf)
-    sudo archlinux-java set $java
-    javac -version
+  find /usr/lib/jvm/java-* > /dev/null 2>&1
+  if test $status -ne 0
+    echo ""
+    echo "No Java SDKs available under /usr/lib/jvm. Please install a JVM at that location"
+    return 1
+  end
+  set java_directories (find /usr/lib/jvm/java-* -maxdepth 0 -type d -printf "%f\n")
+  set java (find /usr/lib/jvm/java-* -maxdepth 0 -type d -printf "%f\n" | fzf)
+  #echo "'$java' count $java"
+  sudo archlinux-java set $java
+  javac -version
 end
