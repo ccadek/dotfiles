2c489f1c4e6beb808b732bdb0a3ffb2f2c64dbc2
Chris Cadek <cc@wh-i.at>
2024-08-02 14:43:56 +0200

    nvim: conform.nvim will now format markdown

--- .config/nvim/lua/plugins/lsp/conform.lua
+++ .config/nvim/lua/plugins/lsp/conform.lua
@@ -26,6 +26,7 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      markdown = { { 'prettierd', 'prettier' } },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
