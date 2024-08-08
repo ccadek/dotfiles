a7e9e97e47be2e20f3e6aea4e8645f7dcea52190
Chris Cadek <cc@wh-i.at>
2024-08-08 13:57:46 +0200

    fish: fixes copy-from-image command

--- .config/fish/functions/containers/copy-from-image.fish
+++ .config/fish/functions/containers/copy-from-image.fish
@@ -1,6 +1,8 @@
 function copy-from-image
     # finish later
-    set id=$(docker create $argv[1])
-    docker cp (id):(argv[2]) - >(argv[3])
-    docker rm -v (id)
+    set container_id $(docker create $argv[1])
+    docker cp $container_id:$argv[2] $argv[3]
+    docker rm -v $container_id
+
+    echo $container_id
 end
