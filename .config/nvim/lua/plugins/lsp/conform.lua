0a723a64eee98d218174c25f63fc823ca2212515
Chris Cadek <cc@wh-i.at>
2024-11-04 10:24:43 +0100

    nvim: conform should format JSON nicely

--- .config/nvim/lua/plugins/lsp/conform.lua
+++ .config/nvim/lua/plugins/lsp/conform.lua
@@ -27,6 +27,7 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       markdown = { 'prettierd', 'prettier' },
+      json = { 'jq' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
