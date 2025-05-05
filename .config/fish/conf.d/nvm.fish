39a9a2898d4721c1b8b03c719655a64c70afba62
Chris Cadek <cc@wh-i.at>
2025-05-05 23:12:55 +0200

    fish: fixes faulty nvm startup

--- .config/fish/conf.d/nvm.fish
+++ .config/fish/conf.d/nvm.fish
@@ -1,6 +1,6 @@
-set --query nvm_mirror || set --global nvm_mirror https://nodejs.org/dist
 set --query XDG_DATA_HOME || set --local XDG_DATA_HOME ~/.local/share
-set --global nvm_data $XDG_DATA_HOME/nvm
+set --query nvm_mirror || set --global nvm_mirror https://nodejs.org/dist
+set --query nvm_data || set --global nvm_data $XDG_DATA_HOME/nvm
 
 function _nvm_install --on-event nvm_install
     test ! -d $nvm_data && command mkdir -p $nvm_data
@@ -23,6 +23,6 @@ function _nvm_uninstall --on-event nvm_uninstall
     functions --erase (functions --all | string match --entire --regex -- "^_nvm_")
 end
 
-if status is-interactive && set --query nvm_default_version && ! set --query nvm_current_version
-    nvm use --silent $nvm_default_version
-end
+# if status is-interactive && set --query nvm_default_version && ! set --query nvm_current_version
+#     nvm use --silent $nvm_default_version
+# end
