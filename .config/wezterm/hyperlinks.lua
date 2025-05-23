656f8678270b9b4a0aa075021852ca9d75e98e4e
Chris Cadek <cc@wh-i.at>
2025-05-23 13:56:12 +0200

    wezterm: make git flow branches clickable

--- .config/wezterm/hyperlinks.lua
+++ .config/wezterm/hyperlinks.lua
@@ -4,20 +4,21 @@ local wezterm = require 'wezterm'
 -- in the browser
 local hyperlink_rules = wezterm.default_hyperlink_rules()
 
--- Handle something that looks like a ticket number clickable.
--- e.g. TICKET-123 will be opened in the browser
+-- Handle something that looks like a feature, release or bugfix branch and link to Jira
+-- e.g. feature/TICKET-123 will be opened in the browser to Jira
 table.insert(hyperlink_rules, {
-  regex = [[\b[A-Z]+-(\d+)\b]],
-  format = 'https://jira.wh-i.at/browse/$0',
+  regex = [[\b(feature\/)([A-Za-z]+(\-)\d+)\b]],
+  format = 'https://jira.wh-i.at/browse/$2',
 })
 
--- make username/project paths clickable. this implies paths like the following are for github.
--- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
--- as long as a full url hyperlink regex exists above this it should not match a full url to
--- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
 table.insert(hyperlink_rules, {
-  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-  format = 'https://www.github.com/$1/$3',
+  regex = [[\b(bugfix\/)([A-Za-z]+(\-)\d+)\b]],
+  format = 'https://jira.wh-i.at/browse/$2',
+})
+
+table.insert(hyperlink_rules, {
+  regex = [[\b(release\/)([A-Za-z]+(\-)\d+)\b]],
+  format = 'https://jira.wh-i.at/browse/$2',
 })
 
 return hyperlink_rules
